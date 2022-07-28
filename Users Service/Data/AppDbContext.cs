624bd3ee6ed4eec8e6a92e5c66517b2394aa3cfb
Prasad Pathak <prasadpathakp20@gmail.com>
2022-07-28 11:59:22 +0530

    Add project files.

---
+++ Users Service/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Users_Service.Entities;
+
+
+namespace UsersService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-0G0Q5V4\SQLEXPRESS;Database=SHAURYA;Integrated Security=True;");
+        }
+    }
+}
+
---
+++ Users Service/Entities/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace Users_Service.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+
+    }
+}
